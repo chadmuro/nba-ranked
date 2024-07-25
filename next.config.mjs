5ac0af84c9968e2a77eecfdd0cf4524c8b21e1bb
chadmuro <chadmuro@gmail.com>
2024-07-25 19:09:27 +0900

    Create api route to get data and install convex

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['www.basketball-reference.com'],
+  }
+};
 
 export default nextConfig;
