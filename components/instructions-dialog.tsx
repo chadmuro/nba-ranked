b75b97a8608aa4fceeae614af5d706eb9878e549
chadmuro <chadmuro@gmail.com>
2024-08-05 23:42:17 +0900

    Add instructions dialog content

--- components/instructions-dialog.tsx
+++ components/instructions-dialog.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -9,8 +11,12 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { useIsMobile } from "@/hooks/useMobile";
+import Image from "next/image";
 
 export default function InstructionsDialog() {
+  const isMobile = useIsMobile();
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -19,7 +25,19 @@ export default function InstructionsDialog() {
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>How to play</DialogTitle>
-          {/* <DialogDescription>Learn how to play NBA, Ranked</DialogDescription> */}
+          <div className="flex flex-col justify-center items-center py-4">
+            <DialogDescription>
+              After submitting your answers, green means the player is in the
+              correct position. Yellow means the player is one spot away. Red
+              means the player is more than one spot away.
+            </DialogDescription>
+            <Image
+              src={isMobile ? "/instructions-mobile.png" : "/instructions.png"}
+              alt="Instructions"
+              height={isMobile ? 600 : 400}
+              width={isMobile ? 200 : 600}
+            />
+          </div>
         </DialogHeader>
 
         <DialogFooter>
